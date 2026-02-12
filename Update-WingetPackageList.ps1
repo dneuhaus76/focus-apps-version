d9b145ec2dcf678d25d049d264a6f86ec2ca5c4c
Daniel Neuhaus <81403092+dneuhaus76@users.noreply.github.com>
2026-02-12 09:32:41 +0100

    Update Update-WingetPackageList.ps1

--- Update-WingetPackageList.ps1
+++ Update-WingetPackageList.ps1
@@ -15,15 +15,15 @@ function Update-WingetPackageList {
     
     # Winget Location & Update
     $appInstaller = Get-AppPackage *Microsoft.DesktopAppInstaller*
-    if ($null -eq $appInstaller) { throw "WinGet (AppInstaller) ist nicht installiert!" }
+    #if ($null -eq $appInstaller) { throw "WinGet (AppInstaller) ist nicht installiert!" }
     Set-Location $appInstaller.InstallLocation
 
-    $ver = & .\winget.exe --version --disable-interactivity
-    Write-Host "winget version $ver before update"
+    #$ver = & .\winget.exe --version --disable-interactivity
+    #Write-Host "winget version $ver before update"
     
     $null = & .\winget.exe source update -n winget --disable-interactivity
     if ($LASTEXITCODE -eq 0) {
-        Write-Host "Winget source update erfolgreich." -ForegroundColor Green
+        #Write-Host "Winget source update erfolgreich." -ForegroundColor Green
     }
   
     if (!(Test-Path $DBFilePath)) { $null = New-Item $DBFilePath -Force -ItemType Directory }
@@ -34,7 +34,7 @@ function Update-WingetPackageList {
 
     # NuGet Provider
     if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
-        Write-Host "Installiere NuGet-Provider..." -ForegroundColor Cyan
+        #Write-Host "Installiere NuGet-Provider..." -ForegroundColor Cyan
         Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ForceBootstrap -Scope Process
     }
 
@@ -45,14 +45,14 @@ function Update-WingetPackageList {
 
     # WinGet-Modul
     if (-not (Get-Module -ListAvailable -Name Microsoft.WinGet.Client)) {
-        Write-Host "Installiere Modul 'Microsoft.WinGet.Client'..." -ForegroundColor Cyan
+        #Write-Host "Installiere Modul 'Microsoft.WinGet.Client'..." -ForegroundColor Cyan
         Install-Module -Name Microsoft.WinGet.Client -Scope Process -Force -AllowClobber
     }
 
     Import-Module Microsoft.WinGet.Client -Force
 
     # --- DATEN EXPORT ---
-    Write-Host "Exportiere alle Pakete (dies kann dauern)..."
+    #Write-Host "Exportiere alle Pakete (dies kann dauern)..."
     # Find-WinGetPackage ohne Query gibt alle verfügbaren Pakete zurück
     $allWingetPackages = Find-WinGetPackage -Source "winget"
     
@@ -60,7 +60,7 @@ function Update-WingetPackageList {
         Sort-Object Name, Version -Descending | 
         Export-Csv -Delimiter "`t" -NoTypeInformation -Path "$DBFilePath\AllWingetPackages.csv" -Encoding UTF8
 
-    Write-Host "File exported: $DBFilePath\AllWingetPackages.csv" -ForegroundColor Green
+    #Write-Host "File exported: $DBFilePath\AllWingetPackages.csv" -ForegroundColor Green
     }
 
 }
