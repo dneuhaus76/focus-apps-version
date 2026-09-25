ec6d46e207b5ede1cbd10c28fac463f0f4e6ead5
Daniel Neuhaus <81403092+dneuhaus76@users.noreply.github.com>
2026-09-25 18:03:50 +0200

    Update Update-WingetPackageList.ps1

--- Update-WingetPackageList.ps1
+++ Update-WingetPackageList.ps1
@@ -18,37 +18,27 @@ function Update-WingetPackageList {
         $null = Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.201" -Scope CurrentUser -Force
     }
 
-    #$msg = Stop-Process -Name WindowsPackageManagerServer -Force
-    #Start-Sleep 2
-    #Write-Output $msg
-
-    #$null = winget install "Microsoft.AppInstaller" --disable-interactivity
-    #$null = winget upgrade "Microsoft.AppInstaller" --disable-interactivity
-    Start-Process pwsh.exe -ArgumentList('-NoProfile -Command "winget install "Microsoft.AppInstaller" --disable-interactivity --force"') -Wait -WindowStyle Hidden
-    #Write-Output $msg
+    Start-Process powershell.exe -ArgumentList('-NoProfile -Command "winget install "Microsoft.AppInstaller" --disable-interactivity --force"') -Wait -WindowStyle Hidden
     $msg = winget source update --disable-interactivity
     Write-Output $msg
 
     if (-not (Get-Module -ListAvailable Microsoft.WinGet.Client)) {
-        #$msg = Install-Module Microsoft.WinGet.Client -Scope AllUsers -AllowClobber -Force
         $msg = Install-Module Microsoft.WinGet.Client -Scope CurrentUser -AllowClobber -Force
         Write-Output $msg
     }
 
-    #$msg = Start-Process pwsh.exe -ArgumentList('-NoProfile -Command "Import-Module Microsoft.WinGet.Client -Force"') -Wait -WindowStyle Hidden
-    #Write-Output $msg
-
-    #Import-Module Microsoft.WinGet.Client -Force
+    Import-Module Microsoft.WinGet.Client -Force
     
     $allWingetPackages = @()
     $allWingetPackages = @(Find-WinGetPackage -Query "$($Query)" -Source "$($Source)" | Select-Object Name, Id, Version, Source -First 1)
     if ($allWingetPackages.Count -eq 0) {
-        #$null = Repair-WinGetPackageManager -Latest -AllUsers -Force
         $null = Repair-WinGetPackageManager -Latest -Force
+    } else {
+        Write-Error "[!] A Test-Package could not be found, so we quit run"
+        Exit 1
     }
     
-    Write-Output $allWingetPackages
-
+    Write-Output "wait while collecting packages..."
     $allWingetPackages = @(Find-WinGetPackage -Query "$($Query)" -Source "$($Source)" | Select-Object Name, Id, Version, Source | Sort-Object Name, Version)
     if ($allWingetPackages.count -gt 0) {
         $allWingetPackages | Export-Csv -Delimiter "`t" -NoTypeInformation -Path "$DBFilePath\$("AllWingetPackages" + ".csv")" -Encoding utf8 -Force
