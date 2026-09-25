e7ac78527a8f2cf298b633c470ff1decf660286a
Daniel Neuhaus <81403092+dneuhaus76@users.noreply.github.com>
2026-09-25 15:41:06 +0200

    Update Update-WingetPackageList.ps1

--- Update-WingetPackageList.ps1
+++ Update-WingetPackageList.ps1
@@ -3,24 +3,37 @@ $ProgressPreference = 'SilentlyContinue'
 
 function Update-WingetPackageList {
     param(
-        [string]$DBFilePath = "$PSScriptRoot\winget-db"
+        [string]$DBFilePath = "$PSScriptRoot\winget-db",
+        [string]$Query = "*",
+        [string]$Source = "winget"
     )
 
     if (!(Test-Path $DBFilePath)) {
         $null = New-Item -ItemType Directory -Path $DBFilePath -Force
     }
 
+    if (-not (Get-PackageProvider "NuGet")) {
+        $null = Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.201" -Force
+    }
+
     $null = winget source update --disable-interactivity
 
     if (-not (Get-Module -ListAvailable Microsoft.WinGet.Client)) {
-        Install-Module Microsoft.WinGet.Client -Scope CurrentUser -Force -AllowClobber
+        $null = Install-Module Microsoft.WinGet.Client -Scope AllUsers -AllowClobber -Force
+        $null = Install-Module Microsoft.WinGet.Client -Scope CurrentUser -AllowClobber -Force
     }
 
-    Import-Module Microsoft.WinGet.Client -Force
+    $null = Import-Module Microsoft.WinGet.Client -Force
 
     $allWingetPackages = @()
-    $allWingetPackages = @(find-WingetPackage -Query "*" -Source "winget" | Select-Object Name, Id, Version, Source | Sort-Object Name, Version)
-    if ($allWingetPackages -and $allWingetPackages.count -gt 0) {
+    $allWingetPackages = @(Find-WinGetPackage -Query "$($Query)" -Source "$($Source)" | Select-Object Name, Id, Version, Source -First 1)
+    if ($allWingetPackages.Count -eq 0) {
+        $null = Repair-WinGetPackageManager -Latest -AllUsers -Force
+        $null = Repair-WinGetPackageManager -Latest -Force
+    }
+
+    $allWingetPackages = @(Find-WinGetPackage -Query "$($Query)" -Source "$($Source)" | Select-Object Name, Id, Version, Source -First 15 | Sort-Object Name, Version)
+    if ($allWingetPackages.count -gt 0) {
         $allWingetPackages | Export-Csv -Delimiter "`t" -NoTypeInformation -Path "$DBFilePath\$("AllWingetPackages" + ".csv")" -Encoding utf8 -Force
         ConvertTo-Json -Depth 5 -InputObject $($allWingetPackages) | Out-File -FilePath $("$DBFilePath\AllWingetPackages.json") -Encoding utf8 -Force
     }
