85d16c640a45565ebadd5fa92b6c61022719d7fc
Daniel Neuhaus <81403092+dneuhaus76@users.noreply.github.com>
2026-09-25 16:54:57 +0200

    Update Update-WingetPackageList.ps1

--- Update-WingetPackageList.ps1
+++ Update-WingetPackageList.ps1
@@ -4,7 +4,7 @@ $ProgressPreference = 'SilentlyContinue'
 function Update-WingetPackageList {
     param(
         [string]$DBFilePath = "$PSScriptRoot\winget-db",
-        [string]$Query = "*",
+        [string]$Query = "",
         [string]$Source = "winget"
     )
 
@@ -17,34 +17,35 @@ function Update-WingetPackageList {
         $null = Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.201" -Scope CurrentUser -Force
     }
 
-    $msg = winget install "Microsoft.AppInstaller" --disable-interactivity
-    Write-Output $msg
-    $msg = winget upgrade "Microsoft.AppInstaller" --disable-interactivity
-    Write-Output $msg
+    $null = winget install "Microsoft.AppInstaller" --disable-interactivity
+    #$null = winget upgrade "Microsoft.AppInstaller" --disable-interactivity
     $msg = winget source update --disable-interactivity
     Write-Output $msg
 
     if (-not (Get-Module -ListAvailable Microsoft.WinGet.Client)) {
-        $null = Install-Module Microsoft.WinGet.Client -Scope AllUsers -AllowClobber -Force
-        $null = Install-Module Microsoft.WinGet.Client -Scope CurrentUser -AllowClobber -Force
+        $msg = Install-Module Microsoft.WinGet.Client -Scope AllUsers -AllowClobber -Force
+        #$msg = Install-Module Microsoft.WinGet.Client -Scope CurrentUser -AllowClobber -Force
+        Write-Output $msg
     }
 
-    $null = Import-Module Microsoft.WinGet.Client -Force
-    $null = Update-Module Microsoft.WinGet.Client -Force
+    $msg = Start-Process pwsh.exe -ArgumentList('-NoProfile -Command "Import-Module Microsoft.WinGet.Client -Force"') -Wait -WindowStyle Hidden
+    Write-Output $msg
 
     $allWingetPackages = @()
     $allWingetPackages = @(Find-WinGetPackage -Query "$($Query)" -Source "$($Source)" | Select-Object Name, Id, Version, Source -First 1)
     if ($allWingetPackages.Count -eq 0) {
         $null = Repair-WinGetPackageManager -Latest -AllUsers -Force
-        $null = Repair-WinGetPackageManager -Latest -Force
+        #$null = Repair-WinGetPackageManager -Latest -Force
     }
     
+    Write-Output $allWingetPackages
 
     $allWingetPackages = @(Find-WinGetPackage -Query "$($Query)" -Source "$($Source)" | Select-Object Name, Id, Version, Source | Sort-Object Name, Version)
-    if ($allWingetPackages -and $allWingetPackages.count -gt 0) {
+    if ($allWingetPackages.count -gt 0) {
         $allWingetPackages | Export-Csv -Delimiter "`t" -NoTypeInformation -Path "$DBFilePath\$("AllWingetPackages" + ".csv")" -Encoding utf8 -Force
         ConvertTo-Json -Depth 5 -InputObject $($allWingetPackages) | Out-File -FilePath $("$DBFilePath\AllWingetPackages.json") -Encoding utf8 -Force
     }
+    Write-Output 'Output in: $("$DBFilePath\AllWingetPackages.json")'
 }
 
 Update-WingetPackageList
